08739d74a2e7084be492d6b3b44dae21c2f91d39
Aung Zin Phyo <aungthurain154@gmail.com>
2022-09-26 15:42:05 +0630

    train commit

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const path = require('path')
+
 const app = express();
+
 
 app.get('',(req,res)=>{
     res.send("<h1>Myanmar Web Developer</h1>")
+})
+
+app.get('/myanmar',function(req,res){
+    res.sendFile(path.join(__dirname,'/index.html'))
 })
 
 
